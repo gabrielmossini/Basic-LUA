d29ee2136ab442a35c3824a50cfbd41f900bc7e0
Gabriel Mossini <ga.mossini@gmail.com>
2022-05-15 16:06:21 -0300

    Basic Addition program in LUA
    EN: Basic Addition program coded in LUA
    PT-BR: Programa básico de soma feito em lua.

---
+++ Soma.lua
@@ -0,0 +1,7 @@
+-- Elabore um algoritemo que leia três números inteiros e mostre o resultado
+
+local   x = 2    --variavel local x
+        y = 2    --variavel local y
+        z = 5    --variavel local z
+
+print("O Resultado e: " .. x + y + z)
